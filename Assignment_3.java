5fa958318e40de11a8c118d2622acedb946e9022
spervez-hub <78657858+spervez-hub@users.noreply.github.com>
2021-02-06 12:51:44 -0500

    Create Assignment_3.java

---
+++ Assignment_3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Assignment_3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		 System.out.print("Enter a integer:");
+		 int number = input.nextInt();
+		 System.out.println(1 + number);
+
+	}
+
+}
