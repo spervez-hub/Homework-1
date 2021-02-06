a8751c230e66b9c0a2615e6c88d33c29fba9e350
spervez-hub <78657858+spervez-hub@users.noreply.github.com>
2021-02-06 12:42:11 -0500

    Create Assignment_1.java

---
+++ Assignment_1.java
@@ -0,0 +1,9 @@
+public class Assignment_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Saneen Pervez");
+		System.out.println("spervez@nyit.edu");
+		System.out.println("#1279146");
+	}
+
+}
